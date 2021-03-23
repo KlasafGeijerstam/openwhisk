757e6f0dfa9e9492999305ee847ac22e82fd4791
Klas <klasafgeijerstam@gmail.com>
2021-03-23 11:26:53 +0100

    Worked on parallel removal

--- modifications/data_logger/src/flatten.rs
+++ modifications/data_logger/src/flatten.rs
@@ -41,6 +41,15 @@ pub enum Node {
     P(u64),
 }
 
+impl Node {
+    fn delay(&self) -> u64 {
+        match self {
+            B(data) => data.iter().map(|(n, p)| (n.delay() as f64 * p) as u64).sum(),
+            other => other.delay()
+        }
+    }
+}
+
 use Node::*;
 
 pub fn has_branch(
@@ -49,7 +58,12 @@ pub fn has_branch(
     graph: &Graph,
     max_length: usize,
 ) -> bool {
-    let paths = all_simple_paths::<Vec<_>, _>(&graph, from_node, to_node, 1, Some(max_length));
+    let paths: Vec<_> =
+        all_simple_paths::<Vec<_>, _>(&graph, from_node, to_node, 1, Some(max_length)).collect();
+
+    if paths.len() < 2 {
+        return false;
+    }
 
     let mut sum = 0.0;
     for p in paths {
@@ -170,9 +184,13 @@ pub fn has_parallel(
     to_node: NodeIndex,
     graph: &mut Graph,
     max_length: usize,
-) -> bool{
+) -> bool {
     let tmp = graph.clone();
     let paths = all_simple_paths::<Vec<_>, _>(&tmp, from_node, to_node, 1, Some(max_length));
+
+    if graph.edges(from_node).map(|e| e.weight()).sum::<f64>() <= 1.0 {
+        return false;
+    }
 
     let mut sum = 0.0;
     for p in paths {
@@ -186,6 +204,11 @@ pub fn has_parallel(
     }
     sum > 1.0
 }
+
+pub fn simple_path_count(graph: &Graph, from: NodeIndex, to: NodeIndex) -> usize {
+    all_simple_paths::<Vec<_>, _>(graph, from, to, 1, None).collect::<Vec<_>>().len()
+}
+
 pub fn remove_parallel(
     from_node: NodeIndex,
     to_node: NodeIndex,
@@ -193,8 +216,33 @@ pub fn remove_parallel(
     max_length: usize,
 ) {
 
-    // find the simple path with the longest execution time
+    let tmp = graph.clone();
+    let paths: Vec<_> = all_simple_paths::<Vec<_>, _>(&tmp, from_node, to_node, 1, Some(max_length)).collect();
     
+    let (_, len) = paths.iter().map(|path| {
+        (path, path.iter().zip(path.iter().skip(1)).fold(0.0, |acc, (&from, &to)| {
+            if acc != 0.0 {
+                // Remove intermideary nodes
+                graph.remove_node(from);
+            }
+
+            let edge = graph.find_edge(from, to).unwrap();
+            let edge_probability = graph[edge];
+            acc + (graph[to].delay() as f64 * edge_probability)
+        }) as u64)
+    }).max_by_key(|(_, length)| {
+        *length
+    }).unwrap();
+    
+    
+    for edge in tmp.edges(from_node) {
+        graph.remove_edge(edge.id());
+    }
+
+    let new_node = graph.add_node(P(len));
+    graph.add_edge(from_node, new_node, 1.0);
+    graph.add_edge(new_node, to_node, 1.0);
+
 }
 
 pub fn remove_parallels(graph: &mut Graph) {
@@ -209,7 +257,6 @@ pub fn remove_parallels(graph: &mut Graph) {
             }
         }
     }
-
 }
 
 #[test]
@@ -260,6 +307,9 @@ pub fn flatten(indices: NodeIndicies, graph: &mut Graph) {
     let new_graph: Graph = StableGraph::new();
 
     // While graph has cycles or branches
+
+    remove_self_loops(graph);
+
     while is_cyclic_directed(&new_graph)
         || all_simple_paths::<Vec<NodeIndex>, _>(
             &new_graph,
@@ -271,10 +321,8 @@ pub fn flatten(indices: NodeIndicies, graph: &mut Graph) {
         .count()
             > 1
     {
-
-        // remove cycles
-
         // remove parallels
+        // de
         // remove branches
         // if has_branch(graph,
     }
