9162a134163247a70a5424456551672307d39569
Klas <klasafgeijerstam@gmail.com>
2021-03-23 14:19:14 +0100

    Worked on cycle-removal

--- modifications/data_logger/src/flatten.rs
+++ modifications/data_logger/src/flatten.rs
@@ -1,12 +1,9 @@
-use crate::{epoch_cache::EpochCache, CallGraph, NodeIndicies};
+use crate::{epoch_cache::EpochCache};
 use std::fmt;
 
-use petgraph::algo::all_simple_paths;
-use petgraph::graph::Frozen;
+use petgraph::algo::{all_simple_paths, astar, has_path_connecting};
 use petgraph::prelude::NodeIndex;
-use petgraph::stable_graph::EdgeReference;
-use petgraph::{algo::*, visit::IntoNodeReferences};
-use petgraph::{stable_graph::StableGraph, visit::EdgeRef, Directed};
+use petgraph::{stable_graph::StableGraph, visit::EdgeRef};
 
 pub type Graph = StableGraph<Node, f64>;
 
@@ -44,24 +41,27 @@ pub enum Node {
 impl Node {
     fn delay(&self) -> u64 {
         match self {
-            B(data) => data.iter().map(|(n, p)| (n.delay() as f64 * p) as u64).sum(),
-            other => other.delay()
+            B(data) => data
+                .iter()
+                .map(|(n, p)| (n.delay() as f64 * p) as u64)
+                .sum(),
+            other => other.delay(),
         }
     }
 }
 
 use Node::*;
 
-pub fn has_branch(
+pub fn has_n_branch_paths(
     from_node: NodeIndex,
     to_node: NodeIndex,
     graph: &Graph,
-    max_length: usize,
+    path_count: usize,
 ) -> bool {
     let paths: Vec<_> =
-        all_simple_paths::<Vec<_>, _>(&graph, from_node, to_node, 1, Some(max_length)).collect();
+        all_simple_paths::<Vec<_>, _>(&graph, from_node, to_node, 1, None).collect();
 
-    if paths.len() < 2 {
+    if paths.len() != path_count {
         return false;
     }
 
@@ -78,18 +78,12 @@ pub fn has_branch(
     sum == 1.0
 }
 
-pub fn remove_branch(
-    from_node: NodeIndex,
-    to_node: NodeIndex,
-    graph: &mut Graph,
-    max_length: usize,
-) {
+pub fn remove_branch(from_node: NodeIndex, to_node: NodeIndex, graph: &mut Graph) {
     let tmp = graph.clone();
 
     // get all simple paths
     let paths: Vec<_> =
-        all_simple_paths::<Vec<NodeIndex>, _>(&tmp, from_node, to_node, 1, Some(max_length))
-            .collect();
+        all_simple_paths::<Vec<NodeIndex>, _>(&tmp, from_node, to_node, 1, None).collect();
 
     // TODO: should be set?
     let mut v: Vec<(Node, f64)> = Vec::new();
@@ -136,20 +130,6 @@ pub fn remove_branch(
     graph.add_edge(b, to_node, 1.0);
 }
 
-pub fn remove_branches(graph: &mut Graph) {
-    let tmp = graph.clone();
-
-    for path_length in 1..graph.node_count() {
-        for from_node in tmp.node_indices() {
-            for to_node in tmp.node_indices() {
-                if has_branch(from_node, to_node, graph, path_length) {
-                    remove_branch(from_node, to_node, graph, path_length);
-                }
-            }
-        }
-    }
-}
-
 pub fn remove_self_loops(graph: &mut Graph) {
     let tmp = graph.clone();
 
@@ -179,16 +159,16 @@ pub fn remove_self_loops(graph: &mut Graph) {
     }
 }
 
-pub fn has_parallel(
+pub fn has_n_parallel_paths(
     from_node: NodeIndex,
     to_node: NodeIndex,
-    graph: &mut Graph,
-    max_length: usize,
+    graph: &Graph,
+    path_count: usize,
 ) -> bool {
     let tmp = graph.clone();
-    let paths = all_simple_paths::<Vec<_>, _>(&tmp, from_node, to_node, 1, Some(max_length));
+    let paths: Vec<_> = all_simple_paths::<Vec<_>, _>(&tmp, from_node, to_node, 1, None).collect();
 
-    if graph.edges(from_node).map(|e| e.weight()).sum::<f64>() <= 1.0 {
+    if graph.edges(from_node).map(|e| e.weight()).sum::<f64>() <= 1.0 || paths.len() != path_count {
         return false;
     }
 
@@ -206,35 +186,37 @@ pub fn has_parallel(
 }
 
 pub fn simple_path_count(graph: &Graph, from: NodeIndex, to: NodeIndex) -> usize {
-    all_simple_paths::<Vec<_>, _>(graph, from, to, 1, None).collect::<Vec<_>>().len()
+    all_simple_paths::<Vec<_>, _>(graph, from, to, 1, None)
+        .collect::<Vec<_>>()
+        .len()
 }
 
-pub fn remove_parallel(
-    from_node: NodeIndex,
-    to_node: NodeIndex,
-    graph: &mut Graph,
-    max_length: usize,
-) {
-
+pub fn remove_parallel(from_node: NodeIndex, to_node: NodeIndex, graph: &mut Graph) {
     let tmp = graph.clone();
-    let paths: Vec<_> = all_simple_paths::<Vec<_>, _>(&tmp, from_node, to_node, 1, Some(max_length)).collect();
-    
-    let (_, len) = paths.iter().map(|path| {
-        (path, path.iter().zip(path.iter().skip(1)).fold(0.0, |acc, (&from, &to)| {
-            if acc != 0.0 {
-                // Remove intermideary nodes
-                graph.remove_node(from);
-            }
+    let paths: Vec<_> = all_simple_paths::<Vec<_>, _>(&tmp, from_node, to_node, 1, None).collect();
 
-            let edge = graph.find_edge(from, to).unwrap();
-            let edge_probability = graph[edge];
-            acc + (graph[to].delay() as f64 * edge_probability)
-        }) as u64)
-    }).max_by_key(|(_, length)| {
-        *length
-    }).unwrap();
-    
-    
+    let (_, len) = paths
+        .iter()
+        .map(|path| {
+            (
+                path,
+                path.iter()
+                    .zip(path.iter().skip(1))
+                    .fold(0.0, |acc, (&from, &to)| {
+                        if acc != 0.0 {
+                            // Remove intermideary nodes
+                            graph.remove_node(from);
+                        }
+
+                        let edge = graph.find_edge(from, to).unwrap();
+                        let edge_probability = graph[edge];
+                        acc + (graph[to].delay() as f64 * edge_probability)
+                    }) as u64,
+            )
+        })
+        .max_by_key(|(_, length)| *length)
+        .unwrap();
+
     for edge in tmp.edges(from_node) {
         graph.remove_edge(edge.id());
     }
@@ -242,20 +224,109 @@ pub fn remove_parallel(
     let new_node = graph.add_node(P(len));
     graph.add_edge(from_node, new_node, 1.0);
     graph.add_edge(new_node, to_node, 1.0);
-
 }
 
 pub fn remove_parallels(graph: &mut Graph) {
     let tmp = graph.clone();
+}
 
-    for path_length in 1..graph.node_count() {
-        for from_node in tmp.node_indices() {
-            for to_node in tmp.node_indices() {
-                if has_parallel(from_node, to_node, graph, path_length) {
-                    remove_parallel(from_node, to_node, graph, path_length);
+
+fn remove_cycle(node: NodeIndex, graph: &mut Graph) {
+    let g = graph.clone();
+    let (_, mut path) = astar(&g, node, |n| n == node, |_| 0, |_| 0)
+        .expect("Could not find a path (cycle) starting with given node");
+
+    let &last_node = path.last().unwrap();
+
+    path.insert(0, node);
+    path.push(node);
+
+    let cycle_probability: f64 = path
+        .iter()
+        .zip(path.iter().skip(1))
+        .map(|(from, to)| {
+            let edge = graph.find_edge(*from, *to).unwrap();
+            graph[edge]
+        })
+        .product();
+
+    let cycle_cost: u64 = path.iter().map(|n| graph[*n].delay()).sum();
+
+    // - remove the edge that causes self-loop
+    let e_index = graph.find_edge(last_node, node).unwrap();
+    let e_weight = graph[e_index];
+    graph.remove_edge(e_index);
+
+    // Update other edges with new probability
+    for edge in g.edges(last_node) {
+        graph.update_edge(
+            edge.source(),
+            edge.target(),
+            edge.weight() / (1.0 - e_weight),
+        );
+        
+    }
+
+    // TODO: Backtrack and remove first non-1.0 edge (and subsequent nodes)
+
+    // - add the additional execution time to the node according to paper
+    graph[last_node] = P(graph[last_node].delay()
+        + ((cycle_probability / (1.0 - cycle_probability)) * (cycle_cost as f64)) as u64);
+}
+
+pub fn flatten(graph: &mut Graph) {
+    remove_self_loops(graph);
+
+    let mut processed = true;
+
+    'outer: while processed {
+        processed = false;
+
+        let tmp = graph.clone();
+        for path_count in 2..tmp.node_count() {
+            for from_node in tmp.node_indices() {
+                for to_node in tmp.node_indices() {
+                    if has_n_parallel_paths(from_node, to_node, graph, path_count) {
+                        remove_parallel(from_node, to_node, graph);
+                        processed = true;
+                        continue 'outer;
+                    }
+
+                    if has_n_branch_paths(from_node, to_node, graph, path_count) {
+                        remove_branch(from_node, to_node, graph);
+                        processed = true;
+                        continue 'outer;
+                    }
                 }
             }
         }
+
+        for node in tmp.node_indices() {
+            if has_path_connecting(&tmp, node, node, None) {
+                remove_cycle(node, graph);
+
+                processed = true;
+                continue 'outer;
+            }
+        }
+    }
+}
+
+#[test]
+fn test_removing_cycles() {
+    let mut graph = StableGraph::new();
+
+    let f1 = graph.add_node(N(100));
+    let f2 = graph.add_node(N(50));
+    let f3 = graph.add_node(N(200));
+
+    graph.add_edge(f1, f2, 1.0);
+    graph.add_edge(f2, f3, 0.2);
+    graph.add_edge(f2, f1, 0.8);
+    
+    for i in 0..10 {
+        assert_eq!(has_n_branch_paths(f1, f2, &graph, i), false);
+        assert_eq!(has_n_parallel_paths(f1, f2, &graph, i), false);
     }
 }
 
@@ -273,7 +344,7 @@ fn test_removing_branches() {
     graph.add_edge(f3, f6, 1.0);
     graph.add_edge(f4, f6, 1.0);
 
-    remove_branches(&mut graph);
+    remove_branch(f2, f6, &mut graph);
 
     assert_eq!(graph.node_count(), 3);
     assert_eq!(graph.edge_count(), 2);
@@ -301,29 +372,4 @@ fn test_removing_self_loops() {
     remove_self_loops(&mut graph);
 
     assert_eq!(graph[a], N(650));
-}
-
-pub fn flatten(indices: NodeIndicies, graph: &mut Graph) {
-    let new_graph: Graph = StableGraph::new();
-
-    // While graph has cycles or branches
-
-    remove_self_loops(graph);
-
-    while is_cyclic_directed(&new_graph)
-        || all_simple_paths::<Vec<NodeIndex>, _>(
-            &new_graph,
-            indices["entry_point"],
-            indices["end_point"],
-            1,
-            None,
-        )
-        .count()
-            > 1
-    {
-        // remove parallels
-        // de
-        // remove branches
-        // if has_branch(graph,
-    }
 }
