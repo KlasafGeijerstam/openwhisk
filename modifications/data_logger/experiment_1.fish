0006d67a3a9e7b17acad20e212235cde5f0fc366
Jakub Jagiello <kubajagiello94@gmail.com>
2021-05-03 20:22:24 +0200

    exp 1 tests

--- modifications/data_logger/experiment_1.fish
+++ modifications/data_logger/experiment_1.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
-for i in (seq 1 100)
+for i in (seq 1 600)
 	wsk action invoke run_application --param-file ../symmetrical-telegram/apps/app10.json --result
-	sleep 20
-	curl localhost:8000/experiment_1 >> res_experiment_1
+	sleep 21
+	curl localhost:8000/experiment_1 >> res_experiment_1.2
 end
