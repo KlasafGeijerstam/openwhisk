a2d43874eec67ba9b9762f9e9c63c36f146c7639
Jakub Jagiello <kubajagiello94@gmail.com>
2021-05-07 18:49:30 +0200

    saving

--- modifications/data_logger/experiment_1.fish
+++ modifications/data_logger/experiment_1.fish
@@ -2,6 +2,6 @@
 
 for i in (seq 1 400)
 	wsk action invoke run_application --param-file ../symmetrical-telegram/apps/app10.json --result
-	sleep 20
-	curl localhost:8000/experiment_1 >> experiments/res_experiment_1
+	sleep 16
+	curl localhost:8000/experiment_1 >> experiments/res_experiment_2_epoch_20
 end
