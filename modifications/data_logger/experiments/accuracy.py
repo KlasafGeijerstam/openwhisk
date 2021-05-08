16ef1aac502ad48143a27a672632b7c6008d9fa6
Jakub <kubajagiello94@gmail.com>
2021-05-08 20:58:55 +0200

    save

---
+++ modifications/data_logger/experiments/accuracy.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+def p(a, b):
+    print(f"\"{str(round(a/b, 2)).replace('.', ',')}\"")
+
+
+for line in stdin:
+    estimation, actual = map(int, line.strip().split(","))
+
+    if estimation > actual:
+        p(actual,estimation)
+    else:
+        p(estimation,actual)
